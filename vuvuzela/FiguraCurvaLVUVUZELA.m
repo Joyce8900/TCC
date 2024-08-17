013bdaa91a48ac03d305d9ce17555998458993f7
Joyce <joycesantos8900@gmail.com>
2024-08-16 23:51:19 -0300

    add: imgs

--- vuvuzela/FiguraCurvaLVUVUZELA.m
+++ vuvuzela/FiguraCurvaLVUVUZELA.m
@@ -1,34 +1,34 @@
-%Aluno: Joyce Oliveira dos Santos
-%Matricula: 20210046341
-%Curso: Sistemas de Informação
-%Orientador: Dr. Márcio Barboza
-%Tema: Remoção de ruídos em sinais unidimensionais usando Variação Total
-
 clc
 clear
 close all
 
-
 load curvaL_vuvuzela.mat
 
-plot(J2,J1,'LineWidth', 2)
+plot(J2, J1, 'LineWidth', 2)
 
-lambdaOtimo = lambda(33)
+indice1 = 33;
+indice2 = 100;
+indice3 = 10;
 
-indice = 33; %
-x_ponto = J2(indice);
-y_ponto = J1(indice);
+x_ponto1 = J2(indice1);
+y_ponto1 = J1(indice1);
+x_ponto2 = J2(indice2);
+y_ponto2 = J1(indice2);
+x_ponto3 = J2(indice3);
+y_ponto3 = J1(indice3);
 hold on
-plot(x_ponto, y_ponto,'*','LineWidth', 2)
+plot(x_ponto1, y_ponto1, '*', 'LineWidth', 2)
+plot(x_ponto2, y_ponto2, '*', 'LineWidth', 2)
+plot(x_ponto3, y_ponto3, '*', 'LineWidth', 2)
 
-texto_anotacao = '  Lambda = 0.35';
-text(x_ponto, y_ponto, texto_anotacao);
-
-
-%title('Curva L da função Degrau', 'FontSize', 14); % Aumenta o tamanho do título
+texto_anotacao1 = '  Lambda = 0.35';
+text(x_ponto1, y_ponto1, texto_anotacao1);
+texto_anotacao2 = '  Lambda = 1';
+text(x_ponto2, y_ponto2, texto_anotacao2);
+texto_anotacao3 = '  Lambda = 0.10';
+text(x_ponto3, y_ponto3, texto_anotacao3);
 
 xlabel('||Dx||_1^1', 'FontSize', 14);
-ylabel('||x - y||_2^2', 'FontSize', 14); %
-
+ylabel('||x - y||_2^2', 'FontSize', 14);
 
 
